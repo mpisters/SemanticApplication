bd6bfa837ef69030feaf0ae6ee5a0b11e3238284
Michelle Pisters <Michelle Pisters>
2016-05-25 15:31:40 +0200

    added tootlip to multiline, data of 2002 is now not available for multiline

--- Multiline/MultiLine.js
+++ Multiline/MultiLine.js
@@ -3,7 +3,7 @@
  */
 let margin = {top: 20, right: 80, bottom: 30, left: 50},
     width = 700 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+    height = 500 - margin.top - margin.bottom;
 
 let x = d3.scale.linear()
     .range([0,width]);
@@ -11,7 +11,7 @@ let x = d3.scale.linear()
 let y = d3.scale.linear()
     .range([height, 0]);
 
-let color_1 = d3.scale.category10();
+let mult_color = d3.scale.category10();
 
 let xAxis = d3.svg.axis()
     .scale(x)
@@ -93,7 +93,7 @@ d3.xml("Multiline/data_alle_jaren.xml", "application/xml", function(xml) {
         let years = data[2];
         let dataList = [];
         for (let i = 0; i < immigrants.length; i++) {
-            dataList.push({'Immigrants': immigrants[i], 'Natives': natives[i], 'year': years[i]})
+            dataList.unshift({'Immigrants': immigrants[i], 'Natives': natives[i], 'year': years[i]})
         }
         return dataList;
     };
@@ -102,9 +102,9 @@ d3.xml("Multiline/data_alle_jaren.xml", "application/xml", function(xml) {
 
     let data = makeData(listOfData);
 
-color_1.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
+mult_color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
     
-    let population = color_1.domain().map(function(name) {
+    let population = mult_color.domain().map(function(name) {
         return {
             name: name,
             values: data.map(function(d) {
@@ -113,18 +113,26 @@ color_1.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; })
         };
     });
 
-    x.domain([2002,2016]);
-    y.domain([
-        d3.min(population, function(c) { return d3.min(c.values, function(v) { return v.total; }); }),
-        d3.max(population, function(c) { return d3.max(c.values, function(v) { return v.total; }); })
-    ]);
-    
+    x.domain([2005,2016]);
+    // y.domain([
+    //     d3.min(population, function(c) { return d3.min(c.values, function(v) { return v.total; }); }),
+    //     d3.max(population, function(c) { return d3.max(c.values, function(v) { return v.total; }); })
+    // ]);
+    y.domain([300000,500000]);
+
+
     svg.append("g")
+        .attr("font-family", "Arial")
+        .attr("font-size", "11px")
+        .attr("font-weight", "700")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height + ")")
         .call(xAxis);
     
     svg.append("g")
+        .attr("font-family", "Arial")
+        .attr("font-size", "11px")
+        .attr("font-weight", "700")
         .attr("class", "y axis")
         .call(yAxis);
     
@@ -136,12 +144,120 @@ color_1.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; })
     item.append("path")
         .attr("class", "line")
         .attr("d", function(d) { return line(d.values); })
-        .style("stroke", function(d) { return color_1(d.name); });
-    
+        .style("stroke", function(d) { return mult_color(d.name); });
+
     item.append("text")
-        .datum(function(d) { return {name: d.name, value: d.values[0]}; })
+        .attr("font-family", "Arial")
+        .attr("font-size", "12px")
+        .attr("font-weight", "700")
+        .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
         .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.total) + ")"; })
         .attr("x", 3)
-        .attr("dy", "0.35em")
+        .attr("dy", ".35em")
         .text(function(d) { return d.name; });
+
+    // append a g for all the mouse over nonsense
+    let mouseG = svg.append("g")
+        .attr("class", "mouse-over-effects");
+
+// this is the vertical line
+    mouseG.append("path")
+        .attr("class", "mouse-line")
+        .style("stroke", "black")
+        .style("stroke-width", "1px")
+        .style("opacity", "0");
+
+// keep a reference to all our lines
+    let lines = document.getElementsByClassName('line');
+
+// here's a g for each circle and text on the line
+    let mousePerLine = mouseG.selectAll('.mouse-per-line')
+        .data(population)
+        .enter()
+        .append("g")
+        .attr("class", "mouse-per-line");
+
+// the circle
+    mousePerLine.append("circle")
+        .attr("r", 7)
+        .style("stroke", function(d) {
+            return mult_color(d.name);
+        })
+        .style("fill", "none")
+        .style("stroke-width", "1px")
+        .style("opacity", "0");
+
+// the text
+    mousePerLine.append("text")
+        .attr("transform", "translate(10,3)");
+
+// rect to capture mouse movements
+    mouseG.append('svg:rect')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('fill', 'none')
+        .attr('pointer-events', 'all')
+        .on('mouseout', function() { // on mouse out hide line, circles and text
+            d3.select(".mouse-line")
+                .style("opacity", "0");
+            d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "0");
+            d3.selectAll(".mouse-per-line text")
+                .style("opacity", "0");
+        })
+        .on('mouseover', function() { // on mouse in show line, circles and text
+            d3.select(".mouse-line")
+                .style("opacity", "1");
+            d3.selectAll(".mouse-per-line circle")
+                .style("opacity", "1");
+            d3.selectAll(".mouse-per-line text")
+                .style("opacity", "1");
+        })
+        .on('mousemove', function() { // mouse moving over canvas
+            let mouse = d3.mouse(this);
+
+            // move the vertical line
+            d3.select(".mouse-line")
+                .attr("d", function() {
+                    let d = "M" + mouse[0] + "," + height;
+                    d += " " + mouse[0] + "," + 0;
+                    return d;
+                });
+
+            // position the circle and text
+            d3.selectAll(".mouse-per-line")
+                .attr("transform", function(d, i) {
+                    let xYear = x.invert(mouse[0]),
+                        bisect = d3.bisector(function(d) {return d.year; }).right;
+                    idx = bisect(d.values, xYear);
+
+                    // since we are use curve fitting we can't relay on finding the points like I had done in my last answer
+                    // this conducts a search using some SVG path functions
+                    // to find the correct position on the line
+                    // from http://bl.ocks.org/duopixel/3824661
+                    let beginning = 0,
+                        end = lines[i].getTotalLength();
+
+                    while (true){
+                        target = Math.floor((beginning + end) / 2);
+                        pos = lines[i].getPointAtLength(target);
+                        if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+                            break;
+                        }
+                        if (pos.x > mouse[0])      end = target;
+                        else if (pos.x < mouse[0]) beginning = target;
+                        else break; //position found
+                    }
+
+                    // update the text with y value
+                    d3.select(this).select('text')
+                        .attr("font-family", "Arial")
+                        .attr("font-size", "12px")
+                        .attr("font-weight", "700")
+                        .text(y.invert(pos.y).toFixed(2));
+
+                    // return position
+                    return "translate(" + mouse[0] + "," + pos.y +")";
+                });
+        });
 });

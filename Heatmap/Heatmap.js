3fc702069aa945b75a7ab3ea7adb6fe1a54e975e
Michelle Pisters <Michelle Pisters>
2016-05-29 17:19:34 +0200

    started with converting heatmap data to d3 usable data

--- Heatmap/Heatmap.js
+++ Heatmap/Heatmap.js
@@ -36,63 +36,175 @@ let timeLabels = vis.selectAll(".timeLabel")
 
 
 
-    d3.tsv('Heatmap/data.tsv',
-        function(d) {
-            return {
-                day: +d.day,
-                hour: +d.hour,
-                value: +d.value
-            };
-        },
-        function(error, data) {
-            let colorScale = d3.scale.quantile()
-                .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
-                .range(colors);
+d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
 
-            let cards = vis.selectAll(".hour")
-                .data(data, function(d) {return d.day+':'+d.hour;});
+    let nodes = d3.select(xml).selectAll("*")[0],
+        links = nodes.slice(1).map(function (d) {
+            return {source: d, value: d.innerHTML};
+        });
 
-            console.log(cards)
+    let valuesBinding = links.map(function (item) {
+        let newData = item.source;
+        let xmlNode = newData.getElementsByTagName('binding');
+        if (xmlNode.length > 1) {
+            let x = xmlNode[1];
+            let valueName = x.getAttribute('name');
+            if (valueName === 'one') {
+                return 1;
+            }
+            else if(valueName === 'two'){
+                return 2;
+            }
+            else{
+                return 3;
+            }
+        }
+    });
+    let DomainsAreasValues = links.map(function (item) {
+        let newData = item.source;
+        let xmlNode = newData.getElementsByTagName('binding');
+        if (xmlNode.length > 1) {
+            let x = xmlNode[0];
+            let y = x.getElementsByTagName('literal');
+            let area = y[0].childNodes[0].nodeValue;
+            let domain = x.getAttribute('name');
+            return [domain, area]
+        }
+    });
 
-            cards.append("title");
 
-            cards.enter().append("rect")
-                .attr("x", function(d) { return (d.hour - 1) * gridSize; })
-                .attr("y", function(d) { return (d.day - 1) * gridSize; })
-                .attr("rx", 4)
-                .attr("ry", 4)
-                .attr("class", "hour bordered")
-                .attr("width", gridSize)
-                .attr("height", gridSize)
-                .style("fill", colors[0]);
+    let FilterArray = function (data) {
+        let filterArray = data.filter(Boolean);
+        return filterArray
+    };
 
-            cards.transition().duration(1000)
-                .style("fill", function(d) { return colorScale(d.value); });
+    let listofDomainAreas = FilterArray(DomainsAreasValues);
+    let ListOfValues = FilterArray(valuesBinding);
 
-            cards.select("title").text(function(d) { return d.value; });
+    let makeList = function (data){
+        let domainList = []
+        let areaList = []
+        for (let i = 0; i < data.length; i++){
+            domainList.push(data[i][0])
+            areaList.push(data[i][1])
+        }
+        return [domainList,areaList]
+    }
 
-            cards.exit().remove();
+    // console.log(ListOfValues);
 
-            let legend = vis.selectAll(".legend")
-                .data([0].concat(colorScale.quantiles()), function(d) { return d; });
+    let Data = []
+    Data.push(makeList(listofDomainAreas)[0])
+    Data.push(makeList(listofDomainAreas)[1])
+    Data.push(ListOfValues)
 
-            legend.enter().append("g")
-                .attr("class", "legend");
+    function listvalues(data){
+        let areaNames  = []
+        for (let i = 0; i < data.length; i++){
+            if (areaNames.indexOf(data[i]) === -1 ){
+                areaNames.push(data[i])
+            }
+        }
 
-            legend.append("rect")
-                .attr("x", function(d, i) { return legendElementWidth * i; })
-                .attr("y", height2)
-                .attr("width", legendElementWidth)
-                .attr("height", gridSize / 2)
-                .style("fill", function(d, i) { return colors[i]; });
+    }
 
-            legend.append("text")
-                .attr("class", "mono")
-                .text(function(d) { return "≥ " + Math.round(d); })
-                .attr("x", function(d, i) { return legendElementWidth * i; })
-                .attr("y", height2 + gridSize);
+    let allAreas = listvalues(Data[1])
+    let allDomains = listvalues(Data[0])
+    //
+    let changeStringtoNumber = function(data,string) {
+        console.log(data.length);
+        let numberList = [];
+        let number = 1;
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] === string) {
+                console.log(data[i])
+                numberList.push(number);
+            }
+            else{
+                number = number +1
+                numberList.push(number)
+                string = data[i]
+            }
+        }
+        return numberList
+    }
 
-            legend.exit().remove();
+    let changeArestoNumber = function(data){
+        let numberList = [];
+        let number = 1;
+        let 
+        for (let i = 0; i < data.length; i++) {
+            if (data[i] === string) {
+                console.log(data[i])
+                numberList.push(number);
+            }
+            else{
+                number = number +1
+                numberList.push(number)
+                string = data[i]
+            }
+        }
+        return numberList
+    }
+
+    console.log(changeArestoNumber(Data[2]))
+    let data2 = []
+    data2.push(changeStringtoNumber(Data[0], "Concentration_immigrants"));
+    data2.push(Data[1]);
+    data2.push(Data[2]);
+
+    console.log(Data[1], "data1--------")
+    let finalData = function(data) {
+        let finalList = [];
+        for (let i = 0; i < data[0].length; i++){
+            finalList.push({'domain': data[0][i], 'area': data[1][i] , 'value': data[2][i]})
+
+        }
+        console.log(finalList)
+    }
+console.log(finalData(data2))
+
+
+
+            // cards.append("title");
+            //
+            // cards.enter().append("rect")
+            //     .attr("x", function(d) { return (d.hour - 1) * gridSize; })
+            //     .attr("y", function(d) { return (d.day - 1) * gridSize; })
+            //     .attr("rx", 4)
+            //     .attr("ry", 4)
+            //     .attr("class", "hour bordered")
+            //     .attr("width", gridSize)
+            //     .attr("height", gridSize)
+            //     .style("fill", colors[0]);
+            //
+            // cards.transition().duration(1000)
+            //     .style("fill", function(d) { return colorScale(d.value); });
+            //
+            // cards.select("title").text(function(d) { return d.value; });
+            //
+            // cards.exit().remove();
+            //
+            // let legend = vis.selectAll(".legend")
+            //     .data([0].concat(colorScale.quantiles()), function(d) { return d; });
+            //
+            // legend.enter().append("g")
+            //     .attr("class", "legend");
+            //
+            // legend.append("rect")
+            //     .attr("x", function(d, i) { return legendElementWidth * i; })
+            //     .attr("y", height2)
+            //     .attr("width", legendElementWidth)
+            //     .attr("height", gridSize / 2)
+            //     .style("fill", function(d, i) { return colors[i]; });
+            //
+            // legend.append("text")
+            //     .attr("class", "mono")
+            //     .text(function(d) { return "≥ " + Math.round(d); })
+            //     .attr("x", function(d, i) { return legendElementWidth * i; })
+            //     .attr("y", height2 + gridSize);
+            //
+            // legend.exit().remove();
 
         });
 

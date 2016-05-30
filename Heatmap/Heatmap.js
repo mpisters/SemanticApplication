1e9d1155402f4798dcf6056860aeef17b70f0e84
Michelle Pisters <Michelle Pisters>
2016-05-30 12:30:27 +0200

    Finished Heatmap

--- Heatmap/Heatmap.js
+++ Heatmap/Heatmap.js
@@ -1,10 +1,10 @@
-let margin2 = { top: 50, right: 0, bottom: 100, left: 30 },
-    width2 = 960 - margin2.left - margin2.right,
-    height2 = 430 - margin2.top - margin2.bottom,
-    gridSize = Math.floor(width2 / 23),
-    legendElementWidth = gridSize*2,
-    buckets = 4,
-    colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"] // alternatively colorbrewer.YlGnBu[9]
+let margin2 = { top: 300, right: 0, bottom: 0, left: 220 },
+    width2 = 1000 - margin2.left - margin2.right,
+    height2 = 600 - margin2.top - margin2.bottom,
+    gridSize = Math.floor(width2 / 22),
+    legendElementWidth = 100,
+    colors3 = ['#edf8fb','#b3cde3','#8c96c6','#88419d'],
+    labels = ['unknown','low','average','high'];
 
 let img = d3.select("#heatmap").append("svg")
     .attr("width", width2 + margin2.left + margin2.right)
@@ -12,12 +12,7 @@ let img = d3.select("#heatmap").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
 
-
-
-
-
 d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
-
     let nodes = d3.select(xml).selectAll("*")[0],
         links = nodes.slice(1).map(function (d) {
             return {source: d, value: d.innerHTML};
@@ -40,6 +35,7 @@ d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
             }
         }
     });
+
     let DomainsAreasValues = links.map(function (item) {
         let newData = item.source;
         let xmlNode = newData.getElementsByTagName('binding');
@@ -52,32 +48,30 @@ d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
         }
     });
 
-
     let FilterArray = function (data) {
-        let filteredArray = data.filter(Boolean);
-        return filteredArray
+        return data.filter(Boolean);
     };
 
     let listofDomainAreas = FilterArray(DomainsAreasValues);
     let ListOfValues = FilterArray(valuesBinding);
 
     let makeList = function (data){
-        let domainList = []
-        let areaList = []
+        let domainList = [];
+        let areaList = [];
         for (let i = 0; i < data.length; i++){
-            domainList.push(data[i][0])
-            areaList.push(data[i][1])
+            domainList.push(data[i][0]);
+            areaList.push(data[i][1]);
         }
         return [domainList,areaList]
-    }
+    };
     
-    let Data = []
-    Data.push(makeList(listofDomainAreas)[0])
-    Data.push(makeList(listofDomainAreas)[1])
-    Data.push(ListOfValues)
+    let Data = [];
+    Data.push(makeList(listofDomainAreas)[0]);
+    Data.push(makeList(listofDomainAreas)[1]);
+    Data.push(ListOfValues);
 
     function listvalues(data){
-        let areaNames  = []
+        let areaNames  = [];
         for (let i = 0; i < data.length; i++){
             if (areaNames.indexOf(data[i]) === -1 ){
                 areaNames.push(data[i])
@@ -87,31 +81,30 @@ d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
         return areaNames
     }
 
-    let allAreas = listvalues(Data[1])
-    let allDomains = listvalues(Data[0])
-
+    let allAreas = listvalues(Data[1]);
+    let allDomains = listvalues(Data[0]);
     let allDomainsNumbers = allAreas.map(function(item) {
         return [item, allAreas.indexOf(item)]
-    })
-    //
-    let changeStringtoNumber = function(data,string) {
+    });
+
+    let changeStringToNumber = function(data,string) {
         let numberList = [];
-        let number = 1;
+        let number = 0;
         for (let i = 0; i < data.length; i++) {
             if (data[i] === string) {
                 numberList.push(number);
             }
             else{
-                number = number +1
-                numberList.push(number)
+                number = number +1;
+                numberList.push(number);
                 string = data[i]
             }
         }
         return numberList
-    }
+    };
 
-    let changeAreastoNumber = function(checkList, data) {
-        let List = []
+    let changeAreasToNumber = function(checkList, data) {
+        let List = [];
         for (let i = 0; i < data.length; i++){
             for (let k = 0; k < checkList.length; k++){
                 if (data[i] === checkList[k][0]){
@@ -120,12 +113,12 @@ d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
             }
         }
         return List
-    }
+    };
 
-    let AreaListNumbers = changeAreastoNumber(allDomainsNumbers, Data[1])
+    let AreaListNumbers = changeAreasToNumber(allDomainsNumbers, Data[1])
 
-    let data2 = []
-    data2.push(changeStringtoNumber(Data[0], "Concentration_immigrants"));
+    let data2 = [];
+    data2.push(changeStringToNumber(Data[0], "Concentration_immigrants"));
     data2.push(AreaListNumbers);
     data2.push(Data[2]);
 
@@ -136,7 +129,7 @@ d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
 
         }
         return finalList
-    }
+    };
 
     let data3 = finalData(data2);
     
@@ -144,29 +137,27 @@ d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
         .data(allDomains)
         .enter().append("text")
         .text(function (d) { return d; })
-        .attr("x", 0)
-        .attr("y", function (d, i) { return i * gridSize; })
+        .attr("x", -50)
+        .attr("y", function (d, i) { return i * gridSize + -10; })
         .style("text-anchor", "end")
-        .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-        .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+        .attr("font-weight", "700")
+        .attr("font-size","13px");
 
     let timeLabels = img.selectAll(".timeLabel")
         .data(allAreas)
         .enter().append("text")
-        .text(function(d) {
-            console.log(d);
-            return d; })
-        .attr("x", function(d, i) {return i * gridSize; })
-        .attr("y", 0)
+        .text(function(d) {return d; })
+        .attr("x", 50)
+        .attr("y", function(d,i){return i*gridSize -15})
         .attr("dy", ".35em")
-        .attr("transform", "translate(" + gridSize / 2 + ", -6) rotate(90)")
-        .style("text-anchor", "start");
-
+        .attr("transform", " rotate(270)")
+        .style("text-anchor", "start")
+        .attr("font-weight", "700")
+        .attr("font-size","13px");
 
     var colorScale = d3.scale.quantile()
-        .domain([0, buckets - 1, d3.max(data3, function (d) {
-            return d.value; })])
-        .range(colors);
+        .domain([0, 3])
+        .range(colors3);
 
     var cards = img.selectAll(".area")
         .data(data3, function(d) {return d.domain+':'+d.area;});
@@ -181,7 +172,7 @@ d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
                 .attr("class", "hour bordered")
                 .attr("width", gridSize)
                 .attr("height", gridSize)
-                .style("fill", colors[0]);
+                .style("fill",  function(d, i) { return colors3[i]; });
             
             cards.transition().duration(1000)
                 .style("fill", function(d) { return colorScale(d.value); });
@@ -191,23 +182,24 @@ d3.xml("Heatmap/Heatmap.xml", "application/xml", function(xml) {
             cards.exit().remove();
             
             let legend = img.selectAll(".legend")
-                .data([0].concat(colorScale.quantiles()), function(d) { return d; });
-            
+                .data(labels);
+
             legend.enter().append("g")
                 .attr("class", "legend");
             
             legend.append("rect")
                 .attr("x", function(d, i) { return legendElementWidth * i; })
-                .attr("y", height2)
+                .attr("y", height2 * 0.70)
                 .attr("width", legendElementWidth)
                 .attr("height", gridSize / 2)
-                .style("fill", function(d, i) { return colors[i]; });
+                .style("fill", function(d, i) { return colors3[i]; });
             
             legend.append("text")
+                .data(labels)
                 .attr("class", "mono")
-                .text(function(d) { return "≥ " + Math.round(d); })
-                .attr("x", function(d, i) { return legendElementWidth * i; })
-                .attr("y", height2 + gridSize);
+                .text(function(d) { return d })
+                .attr("x", function(d, i) { return legendElementWidth * i +25; })
+                .attr("y", height2 * 0.80);
             
             legend.exit().remove();
 
